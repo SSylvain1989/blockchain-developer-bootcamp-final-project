8ed86795d23e4d66cc3c88275a0a23311c652457
SSylvain1989 <sylvain.sigonnez@gmail.com>
2021-11-17 12:17:11 +0100

    build: add structure for the final project

---
+++ schoolManage.sol
@@ -0,0 +1,31 @@
+// import owner contrat and role from openZepplin
+
+// declaration of the structure for a director / teacher / students
+
+// role attributions 
+
+// grade structure
+
+// 2. the director of the school will add teacher
+fonction registerTeacher(address _teacherAddress){
+ // add teacher
+};
+
+// 3. the director of the school will add role add the teacher . 
+fonction giveRoleForTeacher(address _teacherAddress){
+ // add role for teacher
+};
+
+// 4. teacher will be able to add students.
+fonction registerStudent(address _studentAddress, string _nameStudent){
+ // add student
+};
+
+// 5. teacher will be able to add grade to students.
+fonction addGrade(address _studentAddress, uint _grade){
+ // add grade
+};
+
+fonction getGradeFromStudent(address _studentAddress){
+ // get grade from student
+};
