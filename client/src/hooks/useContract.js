39f6f59609cda2a21318e6ae0799931b1e75032a
SSylvain1989 <sylvain.sigonnez@gmail.com>
2021-11-22 18:56:23 +0100

    build: put structure for interact with contract

--- client/src/hooks/useContract.js
+++ client/src/hooks/useContract.js
@@ -1,2 +1,21 @@
+import Web3 from "web3";
+import detectEthereumProvider from "@metamask/detect-provider";
 import schoolManager from '../contracts/SchoolManager.json';
 
+const toWei = Web3.utils.toWei;
+
+const metamaskLogin = async () => {
+  const provider = await detectEthereumProvider();
+  return provider.request({ method: "eth_requestAccounts" });
+};
+
+export default function UseContract() {
+
+ return {
+   
+ //  const contract = new web3.eth.Contract(
+ //   contractInterface.abi,
+ //   "addresContract"
+ // );
+ }
+}
