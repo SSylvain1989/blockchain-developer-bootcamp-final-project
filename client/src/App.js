7fc5a0f5c83aef40bb8b7b548de4de048d8ae068
SSylvain1989 <sylvain.sigonnez@gmail.com>
2021-11-30 00:54:16 +0100

    feat: detect if you are owner of the contract, detect if you switch account

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,11 @@ import React, { useState, useEffect } from "react";
 import { schoolManager } from "./abi/abi";
 import Web3 from "web3";
 import ConnectMetamaskButton from './components/ConnectMetamaskButton'
-import { useWeb3React } from '@web3-react/core';
 import './App.css';
 
 // @notice: web3 is the connection with metamask
 const web3 = new Web3(Web3.givenProvider);
-const contractAddress = "0x450788ba3bFB617632F19A310d608cC6A2a37825";
+const contractAddress = "0x9Bc3ad57d23F81a97edB77473D65800B8222F55c";
 // @notice: contract contain address contract and ABI 
 const contract = new web3.eth.Contract(schoolManager, contractAddress);
 
@@ -23,22 +22,20 @@ function App() {
   const [oneStudentFirstName, setOneStudentFirstName] = useState("");
   const [oneStudentGraduate, setOneStudentGraduate] = useState("");
   const [oneStudentError, setOneStudentError] = useState("");
+
   useEffect(() => {
     addSmartContractListener();
     getStudentCount();
     getListStudents();
-  },[eventMessage]);
+  }, [eventMessage]);
 
-  const { active } = useWeb3React();
 
-  console.log(active);
   function addSmartContractListener() {
     contract.events.LogStudentAdded({}, (error, data) => {
       if (error) {
-        console.log('ici:',error.message);
+        console.log('error on logStudentAdded:', error.message);
       } else {
-        console.log('success student added :',data.returnValues);
-
+        console.log('success student added :', data.returnValues);
         setEventMessage('Student Added 🎉 you can add a grade to him, his address is already filled in')
         setSuccessMessageFirstNameStudent(`First Name : ${data.returnValues.firstName}`);
         setSuccessMessageStudentAddress(`Address : ${data.returnValues.studentAddress.slice(0, 10)}...`);
@@ -47,16 +44,16 @@ function App() {
     });
     contract.events.LogGradeAdded({}, (error, data) => {
       if (error) {
-        console.log('error add grade:',error.message);
+        console.log('error add grade:', error.message);
       } else {
-        console.log('success add grade :',data.returnValues.grade);
+        console.log('success add grade :', data.returnValues.grade);
 
         setEventMessage(`Grade added 🙂 : ${data.returnValues.grade}`)
       }
     });
     contract.events.LogStudentGraduate({}, (error, data) => {
       if (error) {
-        console.log('LogStudentGraduateError:',error.message);
+        console.log('LogStudentGraduateError:', error.message);
       } else {
         console.log('logStudentGraduate typeof', typeof data.returnValues.status);
         console.log('success status student:', data.returnValues.status);
@@ -69,7 +66,7 @@ function App() {
       }
     });
   }
-  
+
   async function addStudent() {
     const accounts = await window.ethereum.enable();
     const account = accounts[0];
@@ -77,28 +74,41 @@ function App() {
       .send({
         from: account,
       })
-      .then(setEventMessage('Confirm transaction on metamask and wait for 2 block ...🙂'))
+      .then(setEventMessage('Confirm transaction on metamask...🙂'))
       .then((receipt) => {
         console.log('receipt addStudent:', receipt)
+        setEventMessage('👉 Transaction is send wait for 2 block ...🙂')
         setOneStudentGrade("");
         setOneStudentFirstName("");
         setOneStudentGraduate("");
       })
       .catch((err) => {
-        if(err.message.includes("This student already exist")){
-          setEventMessage("Error : This student already exist 😥")
+
+        if (err.message.includes("This student already exist")) {
+          console.log('error add student', err)
+          setEventMessage("ERROR❗️ : This student already exist 😥")
           setOneStudentGrade("");
           setOneStudentFirstName("");
           setOneStudentGraduate("");
         }
         else {
-          setEventMessage("Error : Something went wrong try again 😥")
-          setSuccessMessageFirstNameStudent('');
-          setSuccessMessageStudentAddress('');
-          setOneStudentGrade("");
-          setOneStudentFirstName("");
-          setOneStudentGraduate("");
-        } 
+          console.log('error add student', err)
+          console.log('error add student', err.message)
+          if (err.message.includes("User denied transaction")) {
+            setEventMessage("ERROR❗️ : You denied transaction 🙁")
+          }
+          if (err.message.includes("caller is not the owner")) {
+            setEventMessage("ERROR❗️ : You are not the owner of this contract you can\'t add student 🙁")
+          }
+          else {
+            setEventMessage("ERROR❗️ : Something went wrong try again 😥")
+            setSuccessMessageFirstNameStudent('');
+            setSuccessMessageStudentAddress('');
+            setOneStudentGrade("");
+            setOneStudentFirstName("");
+            setOneStudentGraduate("");
+          }
+        }
       })
   };
 
@@ -109,26 +119,42 @@ function App() {
       .send({
         from: account,
       })
+      // .then(    contract.events.LogGradeAdded({}, (error, data) => {
+      //   if (error) {
+      //     console.log('error add grade:',error.message);
+      //   } else {
+      //     console.log('success add grade :',data.returnValues.grade);
+
+      //     setEventMessage(`Grade added 🙂 : ${data.returnValues.grade}`)
+      //   }
+      // }))
       .then(setEventMessage('Confirm transaction on metamask and wait for 2 block ...🙂'))
       .then((receipt) => {
         console.log('receipt addGrade:', receipt)
       })
       .catch((err) => {
-        if(err.message.includes("First you need to add the student")){
-          setEventMessage("Error : First you need to add the student 😥")
+        if (err.message.includes("First you need to add the student")) {
+          setEventMessage("Error : First you need to add the student ☺️")
           setStatusGraduate('');
         }
-        if(err.message.includes("Student already receive grade")){
+        if (err.message.includes("Student already receive grade")) {
           setEventMessage("Error : Student already received grade 😉 you can check his grade with the student section below 👇")
           setStatusGraduate('');
         }
+        if (err.message.includes("User denied transaction")) {
+          setEventMessage("ERROR❗️ : You denied transaction 🙁")
+        }
+        if (err.message.includes("caller is not the owner")) {
+          setEventMessage("ERROR❗️ : You are not the owner of this contract you can\'t add grade 🙁")
+        }
         else {
-          console.log('addGradeErro',err)
-          setEventMessage("Error : Something went wrong try again 😥")
+          console.log('add Grade Error', err.message)
+          console.log('add Grade Error', err)
+          setEventMessage("ERROR❗️: Something went wrong try again 😥")
           setStatusGraduate('');
           setSuccessMessageFirstNameStudent('');
           setSuccessMessageStudentAddress('');
-        } 
+        }
       })
   };
 
@@ -138,15 +164,25 @@ function App() {
         setStatusGraduate('');
         setSuccessMessageFirstNameStudent('');
         setSuccessMessageStudentAddress('');
-        setOneStudentGrade(receipt.grade);
         setOneStudentFirstName(receipt.firstName);
         if (receipt[0] === '1') {
+          setOneStudentGrade(`you got the grade of ${receipt.grade}`);
           setOneStudentGraduate('congrats you are Graduated 🥳');
           setEventMessage('');
         }
         else {
-          setOneStudentGraduate('so sorry you are not Graduated 😔');
-          setEventMessage('');
+          if (receipt.grade === '0') {
+            setOneStudentGrade('');
+            setStatusGraduate('');
+            setOneStudentGraduate('you haven\'t received a grade yet ... keep waiting please 😊');
+            setEventMessage('');
+          }
+          else {
+            console.log('je passe ici')
+            setOneStudentGrade(`you got the grade of ${receipt.grade}`);
+            setOneStudentGraduate(' so sorry you are not Graduated 😔');
+            setEventMessage('');
+          }
         }
         console.log('getOneStudent:', receipt)
         console.log('getOneStudent:', receipt[0])
@@ -164,10 +200,8 @@ function App() {
 
   async function getStudentCount() {
     await contract.methods.getStudentCount().call()
-    .then(receipt => {
+      .then(receipt => {
         const studentCount = document.getElementById('studentCount');
-        console.log('studentCount',studentCount)
-        console.log('studentCount:',typeof receipt)
         studentCount.innerHTML = `${receipt}`;
       })
       .catch(err => {
@@ -181,43 +215,43 @@ function App() {
         let studList = document.getElementById('studList');
         studList.innerHTML = '';
         for (let i = 0; i < receipt.length; i++) {
-          console.log('studentListe:',receipt[i])
           studList.innerHTML = `${studList.innerHTML} ${receipt[i]}<br/><br/>`;
         }
       })
       .catch(err => {
-        alert('erreur', err)
+        alert(' error , maybe you have no gas on this account ? ', err)
       })
   };
 
+
   return (
     <>
-    <div className="header">
-      <div className="headerText">
-      <h1>School Manager</h1>
-      <p className="headerParagraph">You are a teacher of a class</p>
-      <p className="headerParagraph">You can add Student</p>
-      <p className="headerParagraph">Students get Grades from you only</p>
-      <p className="headerParagraph">Students can check their grade and graduate status</p>
-      </div>
-      <div className="headerConnection">
-      <ConnectMetamaskButton />
+      <div className="header">
+        <div className="headerText">
+          <h1>School Manager</h1>
+          <p className="headerParagraph">You are a teacher of a class</p>
+          <p className="headerParagraph">You can add Student</p>
+          <p className="headerParagraph">Students get Grades from you only</p>
+          <p className="headerParagraph">Students can check their grade and graduate status</p>
         </div>
-    </div>
+        <div className="headerConnection">
+          <ConnectMetamaskButton />
+        </div>
+      </div>
       <div className="main">
         <div className="message">
-        {eventMessage.length > 2
+          {eventMessage.length > 2
             ?
-              <p className="registerStudent">{eventMessage}</p>
+            <p className="registerStudent">{eventMessage}</p>
             :
             ''
-            }
-            {successMessageFirstNameStudent.length > 2
+          }
+          {successMessageFirstNameStudent.length > 2
             ?
             <>
-            <p className="registerStudent">{statusGraduate}</p>
-            <p className="registerStudent">{successMessageFirstNameStudent}</p>
-            <p className="registerStudent">{successMessageStudentAddress}</p>
+              <p className="registerStudent">{statusGraduate}</p>
+              <p className="registerStudent">{successMessageFirstNameStudent}</p>
+              <p className="registerStudent">{successMessageStudentAddress}</p>
             </>
             :
             ''
@@ -226,94 +260,94 @@ function App() {
         <div className="card">
           <div className="addTeacher">
 
-          <label htmlFor="">Add Student Form</label>
-          <input
-            id="addressStudent"
-            value={addressStudent}
-            placeholder="Enter Student address account.."
-            autoComplete="off"
-            onChange={e => setAddressStudent(e.target.value)}
-          >
-          </input>
-          <label htmlFor="">First name</label>
-          <input
-            id="firstNameStudent"
-            value={firstName}
-            placeholder="Enter student first name.."
-            autoComplete="off"
-            onChange={e => setFirstName(e.target.value)}
-          >
-          </input>
-          <button onClick={addStudent}>Add Student</button>
-          <br />
+            <label htmlFor="">Add Student Form</label>
+            <input
+              id="addressStudent"
+              value={addressStudent}
+              placeholder="Enter Student address account.."
+              autoComplete="off"
+              onChange={e => setAddressStudent(e.target.value)}
+            >
+            </input>
+            <label htmlFor="">First name</label>
+            <input
+              id="firstNameStudent"
+              value={firstName}
+              placeholder="Enter student first name.."
+              autoComplete="off"
+              onChange={e => setFirstName(e.target.value)}
+            >
+            </input>
+            <button onClick={addStudent}>Add Student</button>
+            <br />
           </div>
 
           <div className="addTeacher">
-          <label htmlFor="">Add Grade Form</label>
-          <input
-            id="addressStudent"
-            value={addressStudent}
-            placeholder="Enter student address account.."
-            autoComplete="off"
-            onChange={e => setAddressStudent(e.target.value)}
-          >
-          </input>
-          <label htmlFor="">Grade</label>
-          <input
-            id="firstNameStudent"
-            type="number" 
-            value={grade}
-            placeholder="Enter student Grade.."
-            autoComplete="off"
-            onChange={e => setGrade(e.target.value)}
-          >
-          </input>
-          <button onClick={addGrade}>Add Grade</button>
-          <br />
+            <label htmlFor="">Add Grade Form</label>
+            <input
+              id="addressStudent"
+              value={addressStudent}
+              placeholder="Enter student address account.."
+              autoComplete="off"
+              onChange={e => setAddressStudent(e.target.value)}
+            >
+            </input>
+            <label htmlFor="">Grade</label>
+            <input
+              id="firstNameStudent"
+              type="number"
+              value={grade}
+              placeholder="Enter student Grade.."
+              autoComplete="off"
+              onChange={e => setGrade(e.target.value)}
+            >
+            </input>
+            <button onClick={addGrade}>Add Grade</button>
+            <br />
           </div>
 
         </div>
         <div className="main">
-        <div className="addTeacher">
-          <p className="registerStudent" >
-            Student count : <span id="studentCount"> </span> 
-          </p>
-          <br />
-          </div>
-        <div className="addTeacher">
-          <h4>You are a Student ? Find if you are Graduated 🙂</h4>
-
-          {oneStudentError.length > 2
-            ?
-              <p className="registerStudent">{oneStudentError}</p>
-            :
-            ''
-            }
-            {oneStudentFirstName.length > 2
-            ?
-            <>
-            <p className="registerStudent">{oneStudentFirstName} , you got the grade {oneStudentGrade} for your work ,{oneStudentGraduate}</p>
-            </>
-            :
-            ''
-          }
-          <input
-            id="addressStudent"
-            value={addressStudent}
-            placeholder="Enter your address account.."
-            autoComplete="off"
-            onChange={e => setAddressStudent(e.target.value)}
-          >
-          </input>
-          <button onClick={getOneStudent}>Let's find out</button>
-          <br />
-          <br />
-          <br />
+          <div className="addTeacher">
+            <p className="registerStudent" >
+              Student count : <span id="studentCount"> </span>
+            </p>
+            <br />
           </div>
           <div className="addTeacher">
-          <p className="registerStudent">List of Students</p>
-          <div id="studList">
+            <h4>You are a Student ? Find if you are Graduated 🙂</h4>
+
+            {oneStudentError.length > 2
+              ?
+              <p className="registerStudent">{oneStudentError}</p>
+              :
+              ''
+            }
+            {oneStudentFirstName.length > 2
+              ?
+              <>
+                <p className="registerStudent">{oneStudentFirstName} , {oneStudentGrade} for your work , {oneStudentGraduate}</p>
+              </>
+              :
+              ''
+            }
+            <input
+              id="addressStudent"
+              value={addressStudent}
+              placeholder="Enter your address account.."
+              autoComplete="off"
+              onChange={e => setAddressStudent(e.target.value)}
+            >
+            </input>
+            <button onClick={getOneStudent}>Let's find out</button>
+            <br />
+            <br />
+            <br />
           </div>
+          <div className="addTeacher">
+            <p className="registerStudent">List of Students</p>
+            <div id="studList">
+            </div>
           </div>
 
         </div>
--- client/src/components/ConnectMetamaskButton.js
+++ client/src/components/ConnectMetamaskButton.js
@@ -1,10 +1,17 @@
-// import React, { useEffect, useState } from 'react';
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import { useWeb3React } from '@web3-react/core';
+import { schoolManager } from "../abi/abi";
+import Web3 from "web3";
 import { injected } from './wallet/connectors';
+import { reduceAddress } from '../util/reduceAddress';
 
-export default function ConnectMetamaskButton() {
-  const { active, account, activate, deactivate } = useWeb3React()
+const web3 = new Web3(Web3.givenProvider);
+const contractAddress = "0x9Bc3ad57d23F81a97edB77473D65800B8222F55c";
+// @notice: contract contain address contract and ABI 
+const contract = new web3.eth.Contract(schoolManager, contractAddress);
+
+export default function ConnectMetamaskButton({text}) {
+  const { activate, deactivate } = useWeb3React()
 
   // @active: is there a wallet actively connected right now?
   // @account: broadcast the blockchain account / address
@@ -12,9 +19,53 @@ export default function ConnectMetamaskButton() {
   // @connector: the current connector ? injected here
   // @activate: is the method to connect the wallet
   // @deactivate: is the method to disconnect the wallet
+  const [isConnected, setIsConnected] = useState("");
+  const [accountNumber, setAccountNumber] = useState("");
+  const [ownerMessage, setOwnerMessage] = useState("");
+  useEffect(() => {
+    checkIfuserIsLog();
+    getTheOwner();
+
+  }, [accountNumber]);
+  async function checkIfuserIsLog() {
+    const accounts = await window.ethereum.enable();
+    const account = accounts[0];
+    if (account.length > 0) {
+      setIsConnected(true);
+      setAccountNumber(accounts[0]);
+    }
+    else {
+      setIsConnected(false);
+    }
+  }
+
+  async function getTheOwner() {
+    const accounts = await window.ethereum.enable();
+    const account = accounts[0];
+    await contract.methods.owner().call()
+      .then(receipt => {
+        if (receipt.toLowerCase() === account) {
+          setOwnerMessage("Wonderfull you are the owner of this contrat 😉")
+        }
+        else {
+          setOwnerMessage("❗️ You are not the owner of this contrat you can only check view function below 😉")
+        }
+      })
+      .catch(err => {
+        alert('error , maybe you have no gas on this account ?  ', err)
+      })
+  };
+
+  window.ethereum.on('accountsChanged', function (accounts) {
+    setAccountNumber(accounts[0]);
+    setIsConnected(true);
+  })
+
   async function connect() {
     try {
       await activate(injected)
+      setIsConnected(true);
+      getTheOwner();
     } catch (error) {
       console.log(error)
     }
@@ -22,19 +73,26 @@ export default function ConnectMetamaskButton() {
   async function disconnect() {
     try {
       deactivate(injected)
+      setIsConnected(false);
+      setOwnerMessage("");
     } catch (error) {
       console.log(error)
     }
   }
+  console.log(isConnected)
   return (
+    <div className="connectDiv">
+      <div className="ownerMessage">
+      <p>{ownerMessage}</p>
+      </div>
     <div className="buttonMetamask">
       <br />
-      {active && activate
+      {isConnected
         ?
         <div>
           <button className="metamaskButton" onClick={disconnect}>Disconnect Metamask</button>
           <br />
-          <span className="metamaskSpan">Connected with : {account.slice(0, 10)}...</span>
+          <span className="metamaskSpan">Connected with {reduceAddress(accountNumber)}</span>
         </div>
         :
         <div>
@@ -42,6 +100,7 @@ export default function ConnectMetamaskButton() {
         </div>
       }
       <br />
+      </div>
     </div>
   )
 
